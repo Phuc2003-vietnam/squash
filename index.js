385ba2d5e4b0d9641115397d21f5ef5795d94d96
Phuc2003-vietnam <110796921+Phuc2003-vietnam@users.noreply.github.com>
2024-10-02 14:07:20 +0700

    squashing squashing
    * squash again
    
    * squash1
    
    * squash2
    
    ---------
    
    Co-authored-by: Phuc2003-vietnam <phuc.nguyen180728@hcmut.edu.vn>

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 console.log("main")
+
+console.log("feature1")
+console.log("feature2")
+console.log("feature3")
+console.log("feature4")
+console.log("feature5")
+console.log("squahs1")
+console.log("squahs2")
+
 console.log("main2")
